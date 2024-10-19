405be8c513eaddf214d7dd51da1be3d0996583db
leoacpr <leo.leroy@ensae.fr>
2024-10-19 13:16:17 +0000

    kaggle data cleaning: removed useless columns for identification

--- Data_cleaning/Kaggle_Data_Cleaning.py
+++ Data_cleaning/Kaggle_Data_Cleaning.py
@@ -8,4 +8,9 @@ language_df = pd.read_csv("/home/onyxia/work/Effects-Language-Diversity/Data_cle
 print(language_df.columns)
 
 # Deletion of the useless columns for language identification (we only keep the Names of the languages)
-# wals_code, iso_code, glottocode, macroarea, countries, countrycodes
+# wals_code, iso_code, glottocode, macroarea, countrycodes
+
+language_df = language_df.drop(columns=['wals_code', 'iso_code', 'glottocode', 'macroarea', 'countrycodes'])
+print(language_df.columns[0:15])
+
+# Deletion of useless grammatical features (this part is the most prone to subjectivity, maybe some columns should be added afterwards)
