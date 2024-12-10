12992a84f1760ddf6fa483c09c25662b0f423bd3
leoacpr <leo.leroy@ensae.fr>
2024-12-10 16:18:46 +0000

    S3 FINALLY WORKING

--- Data_cleaning/planes.py
+++ Data_cleaning/planes.py
@@ -11,21 +11,47 @@ import boto3
 
 import pandas as pd
 
-# Initialiser le client S3
-s3 = boto3.client('s3')
 
-# Paramètres du bucket et fichier
-bucket_name = 'avion-et-meteo'
-file_name = 'T_ONTIME_REPORTING_january.csv'
 
-# Télécharger le fichier et le lire dans Pandas
-s3.download_file(bucket_name, file_name, 'local_file.csv')
-df = pd.read_csv('local_file.csv')
-print(df.head())
+bucket_name = "avion-et-meteo"
+access_key = "cachée"
+secret_key = "cachée"
 
-'''#pip install smart_open
-from smart_open import open
-file_path = f's3://{bucket_name}/{file_name}'
-df = pd.read_csv(open(file_path, 'rb'))
-print(df.head())'''
+# Create a session and S3 client
+session = boto3.session.Session()
+s3_client = session.client(
+    service_name='s3',
+    aws_access_key_id=access_key,
+    aws_secret_access_key=secret_key,
+)
+
+
+# List objects in the specific bucket
+print(f"Listing objects in bucket '{bucket_name}':")
+response = s3_client.list_objects_v2(Bucket=bucket_name)
+if 'Contents' in response:
+    for obj in response['Contents']:
+        print(f"  {obj['Key']}")
+else:
+    print("  Bucket is empty.")
+
+
+
+
+import pandas as pd
+
+# Replace with the path to your CSV file
+file_name = 'T_ONTIME_REPORTING_april.csv'
+local_file_path = 'T_ONTIME_REPORTING_april.csv'
+
+try:
+    # Download the file from S3
+    s3_client.download_file(bucket_name, file_name, local_file_path)
+    print(f"File '{file_name}' downloaded successfully.")
+
+    # Read the CSV file into a DataFrame
+    data = pd.read_csv(local_file_path)
+    print(data.head())  # Print the head of the DataFrame
+except Exception as e:
+    print(f"An error occurred: {e}")
 
