5d8e30c8729f051e61b535ca0523f4ce19210204
leo <leo.donzil@ensae.fr>
2024-12-28 09:47:33 +0000

    import data fixed in ML part

--- 3_Machine_Learning/1_regression_model.py
+++ 3_Machine_Learning/1_regression_model.py
@@ -13,11 +13,60 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from xgboost import XGBRegressor
 import matplotlib.pyplot as plt
 import os
+import s3fs
 
 
-# Load the data
+
+
+#Download the files from leoacpr by typing your SSP Cloud username
+#This can take a while because the files are heavy
+
+fs = s3fs.S3FileSystem(client_kwargs={"endpoint_url": "https://minio.lab.sspcloud.fr"})
+
+MY_BUCKET = "leoacpr"
+source_folder = f"{MY_BUCKET}/diffusion/Pre-processing"
+files_in_source = fs.ls(source_folder)
+
+
+#Downloading the dataframes
+dataframes = {}
+
+for files in fs.ls(f"{MY_BUCKET}/diffusion/Pre-processed_data"):
+    with fs.open(files, "r") as file_in:
+            df_imported = pd.read_csv(file_in)
+            print(f"Downloading {files}")
+    # Dictionnary of dataframes with the name of the file as a key
+    dataframes[f"{files.split('/')[-1]}"] = df_imported
+
+print(dataframes['plane_weather.csv'])
+
+
+#Load the data
+plane_weather = dataframes['plane_weather.csv']
+plane_weather_for_ML = dataframes['plane_weather_for_ML.csv']
+JFK_2017_number = dataframes['JFK_2017_number.csv']
+weather_2017 = dataframes['weather_2017.csv']
+plane_weather.drop(columns=['Unnamed: 0'], inplace=True)
+plane_weather_for_ML.drop(columns=['Unnamed: 0'], inplace=True)
+JFK_2017_number.drop(columns=['Unnamed: 0'], inplace=True)
+weather_2017.drop(columns=['Unnamed: 0'], inplace=True)
+
+
+#STEP 1: Exploring the plane and weather datasets
+
+plane_weather['Full_Departure_Datetime'] = pd.to_datetime(plane_weather['Full_Departure_Datetime'])
+plane_weather['DATE_weather'] = pd.to_datetime(plane_weather['DATE_weather'])
+print(plane_weather.info())
+
+df = plane_weather_for_ML
+
+
+
+
+
+"""# Load the data
 file_path = 'Avions-Retard-et-Meteo/1_Data_cleaning/plane_weather_for_ML.csv'
-df = pd.read_csv(file_path)
+df = pd.read_csv(file_path)"""
 
 # Select features and target
 exclude_columns = ['DEP_DELAY', 'ARR_DELAY', 'CARRIER_DELAY', 'WEATHER_DELAY', 'CANCELLED'] # We can't use these variables in our model because they depend on whether or not the plane has been late or not
