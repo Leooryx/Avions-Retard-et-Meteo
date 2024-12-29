975ee176ceb8e7d986416436915064b22cd37ede
leoacpr <leo.leroy@ensae.fr>
2024-12-29 11:11:12 +0000

    init file creation

--- 1_Data_cleaning/pre-processing.py
+++ 1_Data_cleaning/pre-processing.py
@@ -1,5 +1,7 @@
 #Data-preprocessing
 
+from requirements import requirements
+
 #Download the files from leoacpr by typing your SSP Cloud username
 #This can take a while because the files are heavy
 
